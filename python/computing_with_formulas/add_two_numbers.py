b805b0e094831000ba68fa697d0bf4fb5f06b29f
Nolan <nolanbahr.nb@gmail.com>
2026-02-10 15:03:23 +0000

    finished ex 2

--- python/computing_with_formulas/add_two_numbers.py
+++ python/computing_with_formulas/add_two_numbers.py
@@ -1,1 +1,6 @@
-print(1 + 2)
+first_number = 5.
+# print(f'Type of first number is: {type(first_number)}')
+second_number = 7
+
+summation = first_number + second_number
+print(f'The sum of {first_number} and {second_number} is {summation}.')
