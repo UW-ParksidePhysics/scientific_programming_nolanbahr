9896ed907786413276c2360e1a19b8cf86105cc0
Nolan <nolanbahr.nb@gmail.com>
2026-02-12 18:46:31 +0000

    edited the mini retro 2

--- python/computing_with_formulas/calculate_egg_cook_time.py
+++ python/computing_with_formulas/calculate_egg_cook_time.py
@@ -1,5 +1,10 @@
-mass = 0.06
-k = 5000
+small_egg_mass = 47  # g
+large_egg_mass = 67  # g
+density = 1.038  # g / cm^3
+heat_capacity = 3.7  # J / (g K)
+
+
+
 time = k * mass
 
 print("Egg cook time:", time, "seconds")
