643462d1f1f79526d556cd96ba1127cf03871416
Nolan <nolanbahr.nb@gmail.com>
2026-04-28 19:54:44 +0000

    python matrices

--- python/matrices/fifth_eigenvector.py
+++ python/matrices/fifth_eigenvector.py
@@ -1,1 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+matrix_dimension = 5
+
+H = (1 / (2 * (1 / (matrix_dimension + 1))**2)) * (
+    np.diagflat(2 * np.ones(matrix_dimension)) +
+    np.diagflat(-1 * np.ones(matrix_dimension - 1), 1) +
+    np.diagflat(-1 * np.ones(matrix_dimension - 1), -1)
+)
+
+values, vectors = np.linalg.eig(H)
+
+index = np.argsort(values)
+values = values[index]
+vectors = vectors[:, index]
+
+fifth_vector = vectors[:, matrix_dimension - 1]
+
+x_values = np.linspace(
+    1 / (matrix_dimension + 1),
+    matrix_dimension / (matrix_dimension + 1),
+    matrix_dimension
+)
+
+x = np.linspace(0, 1, 100)
+y = np.sqrt(2) * np.sin(np.pi * x)
+
+plt.plot(x_values, fifth_vector, "o", label="5th eigenvector")
+plt.plot(x, y, label="sqrt(2)sin(pi x)")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.savefig("fifth_eigenvector.png")
+plt.show()
--- python/matrices/tenth_eigenvector.py
+++ python/matrices/tenth_eigenvector.py
@@ -1,1 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+matrix_dimension = 10
+
+H = (1 / (2 * (1 / (matrix_dimension + 1))**2)) * (
+    np.diagflat(2 * np.ones(matrix_dimension)) +
+    np.diagflat(-1 * np.ones(matrix_dimension - 1), 1) +
+    np.diagflat(-1 * np.ones(matrix_dimension - 1), -1)
+)
+
+values, vectors = np.linalg.eig(H)
+
+index = np.argsort(values)
+values = values[index]
+vectors = vectors[:, index]
+
+tenth_vector = vectors[:, matrix_dimension - 1]
+
+x_values = np.linspace(
+    1 / (matrix_dimension + 1),
+    matrix_dimension / (matrix_dimension + 1),
+    matrix_dimension
+)
+
+x = np.linspace(0, 1, 100)
+y = np.sqrt(2) * np.sin(np.pi * x)
+
+plt.plot(x_values, tenth_vector, "o", label="10th eigenvector")
+plt.plot(x, y, label="sqrt(2)sin(pi x)")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.savefig("tenth_eigenvector.png")
+plt.show()
